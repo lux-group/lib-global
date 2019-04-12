30d2791880b846fc85ee2b81ee1300e65c0c3222
Kristina Stefanova <kristina.stefanova@luxgroup.com>
2019-04-12 10:25:21 +1000

    Add new LE vendor ID

--- compiled/vendor/index.js
+++ compiled/vendor/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var LE_TOURS_VENDOR_IDS = ['00128000018BZkIAAW', '0010I00001ba7JdQAI', '0010I00001f6bHvQAI'];
+var LE_TOURS_VENDOR_IDS = ['00128000018BZkIAAW', '0010I00001ba7JdQAI', '0010I00001f6bHvQAI', '0010I000028DrsxQAC'];
 
 function requiresTravellerDetails(vendorId) {
   return LE_TOURS_VENDOR_IDS.includes(vendorId);

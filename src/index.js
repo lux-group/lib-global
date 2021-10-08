4c35d3f1b22a42bd1e212ce5c9e92401fb8c92e7
Kostya Bartchenkov <konstantin.bartchenkov@luxuryescapes.com>
2021-10-08 14:22:15 +1100

    FLY-672: Updated export

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   vendor: require('./vendor'),
   flights: require('./flights'),
+  carriers: require('./flights/carriers'),
   offer: require('./offer'),
   currency: require('./currency'),
 }

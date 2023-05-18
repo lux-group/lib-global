0ef6b28a5644ca8d450753a820d302254ab3b942
Cleber Ricardi <cleberricardi@outlook.com>
2023-05-18 19:04:03 -0300

    add source for dynamic tags

---
+++ src/white-label/constants.js
@@ -0,0 +1,62 @@
+const dynamicValuesSource = {
+  treatmetravel: {
+    BrandName: 'Treat Me',
+    SalesEmail: 'sales@luxuryescapes.com',
+    CruiseEmail: 'cruises@luxuryescapes.com',
+    TourEmail: 'tours@luxuryescapes.com',
+    TrustedPartnerTourEmail: 'trustedpartnertours@luxuryescapes.com',
+    FlightPolicyLink: 'https://luxeveryday.com/treatme-travel-terms-and-conditions',
+  },
+  dealstravel: {
+    BrandName: 'Deals',
+    SalesEmail: 'sales@luxuryescapes.com',
+    CruiseEmail: 'cruises@luxuryescapes.com',
+    TourEmail: 'tours@luxuryescapes.com',
+    TrustedPartnerTourEmail: 'trustedpartnertours@luxuryescapes.com',
+    FlightPolicyLink: 'https://luxeveryday.com/deals-travel-terms-and-conditions',
+  },
+  cudotravel: {
+    BrandName: 'Cudo',
+    SalesEmail: 'sales@luxuryescapes.com',
+    CruiseEmail: 'cruises@luxuryescapes.com',
+    TourEmail: 'tours@luxuryescapes.com',
+    TrustedPartnerTourEmail: 'trustedpartnertours@luxuryescapes.com',
+    FlightPolicyLink: 'https://luxeveryday.com/cudo-travel-terms-and-conditions',
+  },
+  scoopontravel: {
+    BrandName: 'Scoopon',
+    SalesEmail: 'sales@luxuryescapes.com',
+    CruiseEmail: 'cruises@luxuryescapes.com',
+    TourEmail: 'tours@luxuryescapes.com',
+    TrustedPartnerTourEmail: 'trustedpartnertours@luxuryescapes.com',
+    FlightPolicyLink: 'https://luxeveryday.com/scoopon-travel-terms-and-conditions',
+  },
+  kogantravel: {
+    BrandName: 'Kogan Travel',
+    SalesEmail: 'sales@kogantravel.com',
+    CruiseEmail: 'sales@kogantravel.com',
+    TourEmail: 'sales@kogantravel.com',
+    TrustedPartnerTourEmail: 'sales@kogantravel.com',
+    FlightPolicyLink: 'https://kogantravel.com/au/flights-policies',
+  },
+  lebusinesstraveller: {
+    BrandName: 'LE for Business',
+    SalesEmail: 'sales@luxuryescapes.com',
+    CruiseEmail: 'cruises@luxuryescapes.com',
+    TourEmail: 'tours@luxuryescapes.com',
+    TrustedPartnerTourEmail: 'trustedpartnertours@luxuryescapes.com',
+    FlightPolicyLink: 'https://luxuryescapes.com/au/flights-policies',
+  },
+  luxuryescapes: {
+    BrandName: 'Luxury Escapes',
+    SalesEmail: 'sales@luxuryescapes.com',
+    CruiseEmail: 'cruises@luxuryescapes.com',
+    TourEmail: 'tours@luxuryescapes.com',
+    TrustedPartnerTourEmail: 'trustedpartnertours@luxuryescapes.com',
+    FlightPolicyLink: 'https://luxuryescapes.com/au/flights-policies',
+  },
+};
+
+module.exports = {
+  dynamicValuesSource
+}

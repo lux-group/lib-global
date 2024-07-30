9155571f5feed5bd07468054f479480715b70082
Tuan Hung Pham <tuanhung.pham@luxuryescapes.com>
2024-07-30 13:30:52 +1000

    fix: backward compability

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,7 +1,13 @@
-module.exports = {
+const environment = {
   DEVELOPMENT: 'development',
   SPEC: 'spec',
   TEST: 'test',
   PRODUCTION: 'production',
   PERFORMANCE: 'performance',
+}
+
+export default {
+  ...environment,
+  // @deprecated kept for backward compability, not supported by index.d.ts
+  environment,
 }

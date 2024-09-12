194ffb5ec32acbe59298bad7b68fb88a64451f63
Semeer Acheerithodi <semeer.acheerithodi@luxuryescapes.com>
2024-09-12 18:08:26 +1000

    add flash cruise vendors

--- src/vendor/index.js
+++ src/vendor/index.js
@@ -10,6 +10,20 @@ function requiresTravellerDetails(vendorId) {
   return LE_TOURS_VENDOR_IDS.includes(vendorId)
 }
 
+// vendors served by svc-tour
+const FLASH_CRUISE_VENDORS = [
+  '0010I000027LvRJQA0',  // Norwegian Cruise Line
+  '0012y00000fiCfEAAU',  // Riverside
+  '0010I000021BD5dQAG',  // Quark Expeditions
+  '0012800000cvFemAAE',  // Coral Expeditions
+  '0012y00000BKJ04AAH',  // Uniworld
+]
+
+function isFlashCruiseVendor(vendorId) {
+  return FLASH_CRUISE_VENDORS.includes(vendorId)
+}
+
 module.exports = {
   requiresTravellerDetails: requiresTravellerDetails,
+  isFlashCruiseVendor: isFlashCruiseVendor,
 }
